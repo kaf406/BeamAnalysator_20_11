af821aba4ec246700cae5a72846e659887047b90
Infarh <shmachilin@yandex.ru>
2020-11-11 13:54:20 +0300

    Чтение данных из файла и запись обработанных результатов

--- BeamAnalysator/Program.cs
+++ BeamAnalysator/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 
 namespace BeamAnalysator
 {
     class Program
     {
+        private const string DataFileName = "BeamPattern_3GHz.txt";
+
         static void Main(string[] args)
         {
-            
+            if (!File.Exists(DataFileName))
+            {
+                Console.WriteLine("Файл данных не найден");
+                return;
+            }
+
+            using StreamReader reader = File.OpenText(DataFileName);
+
+            if (!reader.EndOfStream) reader.ReadLine();
+            if (!reader.EndOfStream) reader.ReadLine();
+
+            List<PatternValue> values = new List<PatternValue>();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+
+                if (line == "")
+                    continue;
+
+                string[] components = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (components.Length < 3)
+                    continue;
+
+                double theta = double.Parse(components[0], CultureInfo.InvariantCulture);
+                double phi = double.Parse(components[1], CultureInfo.InvariantCulture);
+                double dir_db = double.Parse(components[2], CultureInfo.InvariantCulture);
+
+                values.Add(new PatternValue
+                {
+                    Theta = theta,
+                    Phi = phi,
+                    Dir_db = dir_db
+                });
+            }
+
+            const double wanted_phi = 65;
+
+            List<PatternValue> values_65 = new List<PatternValue>();
+
+            for (var i = 0; i < values.Count; i++)
+            {
+                var value = values[i];
+                if (value.Phi == wanted_phi || value.Phi == wanted_phi + 180)
+                    values_65.Add(value);
+            }
+
+            using var writer = File.CreateText("Pattern65.txt");
+
+            writer.WriteLine("ДН для угла 65 градусов");
+            writer.WriteLine("Theta[deg]|F[-]");
+            writer.WriteLine("-----------------------");
+
+            for (var i = 0; i < values_65.Count; i++)
+            {
+                var value = values_65[i];
+
+                writer.WriteLine("{0}\t{1}", value.Theta, Math.Pow(10, value.Dir_db / 20));
+            }
+
+            //Console.ReadLine();
         }
+    }
+
+    struct PatternValue
+    {
+        public double Theta;
+        public double Phi;
+        public double Dir_db;
     }
 }
